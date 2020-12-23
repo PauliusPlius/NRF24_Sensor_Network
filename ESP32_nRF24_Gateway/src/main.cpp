080a1df6d357f3b58d2eb561913c2fc07fff769e
PauliusPlius <PauliusPlius@users.noreply.github.com>
2020-12-23 23:22:41 +0200

    New project

---
+++ ESP32_nRF24_Gateway/src/main.cpp
@@ -0,0 +1,31 @@
+//++++++++++++++++++++++++++++++++++++++++++++++//
+//+++++++++++ ESP32 + nRF24L01 gateway +++++++++//
+//++++++++++++++++++++++++++++++++++++++++++++++//
+
+
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
